4bf578125785aeeb470313156e8e8dd7d5afd66b
William LeGate <william@legate.me>
2018-05-21 14:45:10 -0700

    Restructures project structure (renames server.js to app.js and moved into server directory)

--- server/db.js
+++ server/db.js
@@ -1,17 +1,13 @@
-const pg = require("pg");
-postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...]
+const pg = require('pg');
 
 const credentials = {
-  user: 'aupadlon',
-  pass: 'R9jDOCvYOaWQN_KEVFDez3UOVzV2tRIb',
   host: 'elmer.db.elephantsql.com',
-  port: '5432',
-  dbName: 'aupadlon'
-}
-
-const connectionString = `postgres://${credentials.user}:${credentials.pass}@${credentials.host}:${credentials.port}/${credentials.dbName}`;
-const client = new pg.Client({ connectionString });
-
-module.exports = {
-  client
+  port: 5432,
+  user: 'aupadlon',
+  password: 'R9jDOCvYOaWQN_KEVFDez3UOVzV2tRIb',
+  database: 'aupadlon'
 };
+
+const client = new pg.Client(credentials);
+
+module.exports = client;
