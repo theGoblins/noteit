038b2e8e998f7e0d0f0095d0387003e1ab9d7f20
William LeGate <william@legate.me>
2018-05-22 18:50:25 -0700

    Adds dummy endpoint for returning notes data

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const db = require('./db');
 const noteController = require('./controllers/noteController');
-const userController = require('./controllers/userController');
+const accountController = require('./controllers/accountController');
 
 const app = express();
 
@@ -29,9 +29,56 @@ app.get('/', (req, res) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+////////////////////////////// NEW ROUTES //////////////////////////////
+
+app.get('/notes', (req, res) => {
+
+});
+
+app.get('/notes/:id', (req, res) => {
+  res.json({
+    '_id': 0,
+    url: 'file:///Users/jessica/Desktop/untitled%20folder/test.html',
+    text: 'This is a sample note',
+    startPath: ['html', 'body:eq(3)', 'p'],
+    startIndex: 26,
+    stopPath: ['html', 'body:eq(4)', 'p'],
+    stopIndex: 42,
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+    createdBy: 0,
+  });
+});
+
+app.get('/notes/:url', (req, res) => {
+
+});
+
+app.post('/notes', (req, res) => {
+
+});
+
+app.delete('/notes/:id', (req, res) => {
+
+});
+
+app.post('/accounts/sign-in', (req, res) => {
+
+});
+
+app.post('/accounts/sign-up', (req, res) => {
+
+});
+
+////////////////////////////// OLD ROUTES //////////////////////////////
+
 // User routing
-app.post('/signup', userController.verifyUsername, userController.createUser);
-app.post('/login', userController.verifyUser);
+app.post(
+  '/signup',
+  accountController.verifyUsername,
+  accountController.createUser
+);
+app.post('/login', accountController.verifyUser);
 
 // Note routing
 // get all notes (for testing purposes)
